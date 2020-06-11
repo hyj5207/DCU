4aacc2b673010a8ac8c7d9a2814055aefebe39bf
Ubuntu <ubuntu@ip-172-31-29-146.us-east-2.compute.internal>
2020-06-11 10:26:22 +0000

    add haddp_install.sh

---
+++ hadoop_install.sh
@@ -0,0 +1,283 @@
+#!/bin/bash
+
+
+
+sudo apt update
+
+
+
+echo "-----------------UPDATED-----------------"
+
+
+
+sudo apt install openjdk-8-jdk
+
+java -version
+
+
+
+echo "-------------JAVA INSTALLED--------------"
+
+
+
+wget http://mirror.navercorp.com/apache/hadoop/common/hadoop-2.9.2/hadoop-2.9.2.tar.gz
+
+tar xvfz hadoop-2.9.2.tar.gz
+
+
+
+mkdir ~/apps
+
+mv hadoop-2.9.2 ~/apps
+
+cd ~/apps
+
+ln -s hadoop-2.9.2 hadoop
+
+
+
+echo "-------------HADOOP DOWNLOADED-----------"
+
+
+
+sudo groupadd hadoop
+
+sudo usermod -a -G hadoop ubuntu
+
+
+
+echo "-------------HADOOP USER ADDED-----------"
+
+
+
+cat<<\EDF>>~/.bashrc
+
+#HADOOP VARIABLES START
+
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+
+export HADOOP_HOME=/home/ubuntu/apps/hadoop
+
+export PATH=$PATH:$HADOOP_HOME/bin
+
+export PATH=$PATH:$HADOOP_HOME/sbin
+
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+
+export YARN_HOME=$HADOOP_HOME
+
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+
+export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"
+
+#HADOOP VARIABLES END
+
+EDF
+
+
+
+#source ~/.bashrc
+
+eval "$(cat ~/.bashrc | tail -n +12)"
+
+
+
+hadoop version
+
+
+
+cat<<\EDF>$HADOOP_HOME/etc/hadoop/core-site.xml
+
+<?xml version="1.0" encoding="UTF-8"?>
+
+<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
+
+<!--
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+
+  you may not use this file except in compliance with the License.
+
+  You may obtain a copy of the License at
+
+
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+
+
+  Unless required by applicable law or agreed to in writing, software
+
+  distributed under the License is distributed on an "AS IS" BASIS,
+
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+  See the License for the specific language governing permissions and
+
+  limitations under the License. See accompanying LICENSE file.
+
+-->
+
+
+
+<!-- Put site-specific property overrides in this file. -->
+
+
+
+<configuration>
+
+        <property>
+
+                <name>fs.defaultFS</name>
+
+                <value>hdfs://localhost:9000</value>
+
+        </property>
+
+</configuration>
+
+EDF
+
+
+
+cat<<\EDF>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
+
+<?xml version="1.0" encoding="UTF-8"?>
+
+<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
+
+<!--
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+
+  you may not use this file except in compliance with the License.
+
+  You may obtain a copy of the License at
+
+
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+
+
+  Unless required by applicable law or agreed to in writing, software
+
+  distributed under the License is distributed on an "AS IS" BASIS,
+
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+  See the License for the specific language governing permissions and
+
+  limitations under the License. See accompanying LICENSE file.
+
+-->
+
+
+
+<!-- Put site-specific property overrides in this file. -->
+
+
+
+<configuration>
+
+        <property>
+
+                <name>dfs.replication</name>
+
+                <value>1</value>
+
+        </property>
+
+</configuration>
+
+EDF
+
+
+
+cat<<\EDF>$HADOOP_HOME/etc/hadoop/mapred-site.xml
+
+<configuration>
+
+	<property>
+
+		<name>mapreduce.framework.name</name>
+
+		<value>yarn</value>
+
+	</property>
+
+</configuration>
+
+EDF
+
+
+
+cat<<\EDF>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+
+<?xml version="1.0"?>
+
+<!--
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+
+  you may not use this file except in compliance with the License.
+
+  You may obtain a copy of the License at
+
+
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+
+
+  Unless required by applicable law or agreed to in writing, software
+
+  distributed under the License is distributed on an "AS IS" BASIS,
+
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+  See the License for the specific language governing permissions and
+
+  limitations under the License. See accompanying LICENSE file.
+
+-->
+
+<configuration>
+
+        <property>
+
+                <name>yarn.nodemanager.aux-services</name>
+
+                <value>mapreduce_shuffle</value>
+
+        </property>
+
+</configuration>
+
+EDF
+
+
+
+cat<<\EDF>>$HADOOP_HOME/etc/hadoop/hadoop-env.sh
+
+#export JAVA_HOME=${JAVA_HOME}
+
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+
+EDF
+
+
+
+
+
+ssh-keygen -t rsa
+
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
+ssh localhost
+
+
